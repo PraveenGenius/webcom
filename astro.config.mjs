9d8840d08df1bbf8d524467c36bf62f39985fd0f
Praveen <81355089+PraveenGenius@users.noreply.github.com>
2022-11-12 20:23:50 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import mdx from '@astrojs/mdx';
-import vercel from '@astrojs/vercel/serverless';
-import image from '@astrojs/image';
-import partytown from '@astrojs/partytown';
-import netlify from '@astrojs/netlify/functions';
-
-// https://astro.build/config
-export default defineConfig({
-  integrations: [tailwind(), sitemap(), mdx(), image(), partytown()],
-  output: "server",
-  adapter: netlify()
-});
+export default defineConfig({});
