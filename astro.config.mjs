c194f3c314c64840466fc8caa97fa7830ad4c171
Praveen <81355089+PraveenGenius@users.noreply.github.com>
2022-11-12 20:12:25 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import mdx from "@astrojs/mdx";
 import vercel from "@astrojs/vercel/serverless";
 import image from "@astrojs/image";
 import partytown from "@astrojs/partytown";
-import netlify from "@astrojs/netlify/functions";
+import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
